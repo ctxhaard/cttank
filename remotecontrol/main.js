4bd403e7f093bf8419455030ee84d91da9f396d7
Carlo Tomasin <c.tomasin@gmail.com>
2015-05-29 00:20:43 +0200

    Heading data forwarded to a web page by WebSocket; Added dependency to Socket.io and Express

--- remotecontrol/main.js
+++ remotecontrol/main.js
@@ -1,26 +1,56 @@
-/**
- * New node file
- */
-var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
+#!/usr/bin/env node --harmony
 
-btSerial.on('found',function(address,name){
-    btSerial.findSerialPortChannel(address,function(channel){
-        btSerial.connect(address,channel,function(){
-            console.log('connected');
-            
-            btSerial.on('data',function(buffer){
-                console.log(buffer.toString('utf-8'));
-            });
-        }
-        ,function(){
-            console.log('cannot connect');
-        });
-        //btSerial.close();
-    },function(){
-        console.log('found nothing');
+"use strict"
+
+// TODO: create a WebSocket server
+// - accept a connection
+// - connect to Bt device
+// - pipe input from Rfcomm to WebSocket
+const btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort(),
+    targetBtName = 'MAX MERDA';
+
+const app = require('express')(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
+    
+app.get('/',function(req,res){
+    res.sendFile('index.html',{root: '.'});
+});
+
+io.on('connection',function(socket){
+    console.log('a user connected');
+    
+    console.log('inquiring...')
+    btSerial.inquire();
+    
+    socket.on('disconnect',function(){
+        console.log('user disconnected');
+        // TODO: disconnect Bt
     });
 });
 
-console.log('inquiring...')
-btSerial.inquire();
-console.log('...end!')
+http.listen(3000,function(){
+    console.log('listening on *:3000');
+});
+
+btSerial.on('found',function(address,name){
+    if(name === targetBtName){
+        btSerial.findSerialPortChannel(address,function(channel){
+            btSerial.connect(address,channel,function(){
+                console.log('connected');
+                
+                btSerial.on('data',function(buffer){
+                    console.log(buffer.toString('utf-8'));
+                    io.emit('msg',buffer.toString('utf-8'));
+                });
+            }
+            ,function(){
+                console.log('cannot connect');
+            });
+            //btSerial.close();
+        },function(){
+            console.log('found nothing');
+        });
+    }
+});
+
